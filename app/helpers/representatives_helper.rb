f1f3846dbef1d887489b566c46e89b8641debbf6
allen <allen.liu@berkeley.edu>
2026-08-05 01:12:02 -0700

    feature: added representative profile page

--- app/helpers/representatives_helper.rb
+++ app/helpers/representatives_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module RepresentativesHelper
+  def twitter_url(handle)
+    "https://twitter.com/#{handle.to_s.delete_prefix('@')}"
+  end
+
+  def facebook_url(handle)
+    "https://www.facebook.com/#{handle}"
+  end
+
+  def youtube_url(handle)
+    "https://www.youtube.com/#{handle}"
+  end
 end
