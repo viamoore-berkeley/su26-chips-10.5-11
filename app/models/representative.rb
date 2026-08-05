1a7df578fab5a6d40573e7a58ec8bd5e0f5a36c7
allen <allen.liu@berkeley.edu>
2026-08-04 23:26:20 -0700

    feat: populate representative details from Geocodio response

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -24,11 +24,13 @@
 class Representative < ApplicationRecord
   has_many :news_items, dependent: :delete_all
 
+  PHOTO_URL_TEMPLATE = 'https://bioguide.congress.gov/bioguide/photo/%s/%s.jpg'
+
   # Review the Geocodio docs
   # https://www.geocod.io/docs/#congressional-districts
   def self.geocodio_search(query)
     geocodio_api_key = ENV.fetch('GEOCODIO_API_KEY', Rails.application.credentials[:GEOCODIO_API_KEY])
-    raise ArgumentError 'Missing GEOCODIO_API_KEY' if geocodio_api_key.blank?
+    raise ArgumentError, 'Missing GEOCODIO_API_KEY' if geocodio_api_key.blank?
 
     geocodio = Geocodio::Gem.new(geocodio_api_key)
     geocodio.geocode(query, ['cd'])
@@ -39,36 +41,50 @@ class Representative < ApplicationRecord
   # matching representatives for a search / county.
   # See https://www.geocod.io/docs/#data-appends-fields
   def self.civic_api_to_representative_params(rep_info)
-    reps = []
     response = rep_info['results'][0]['response']
     fields = response['results'][0]['fields']
-    @legislators = fields['congressional_districts'][0]['current_legislators']
-
-    @legislators.each_with_index do |official, _index|
-      official['name'] = "#{official.dig('bio', 'first_name')} #{official.dig('bio', 'last_name')}"
-      title = official['type']
-      # Inspect all the data that's there to make part 1 easier.
-      # Rails.logger.debug official
-      # official.dig('bio', 'party')
-      ocdid = official['govtrack_id']
-      reps << Representative.find_rep(official, ocdid: ocdid, title: title)
-    end
-    reps
+    legislators = fields['congressional_districts'][0]['current_legislators']
+    legislators.map { |official| find_rep(official) }
   end
 
-  def self.find_rep(official, title: '', ocdid: '')
-    rep = Representative.create({ name: official['name'], ocdid: ocdid,
-      title: title, party: official['party'], photo_url: official['photo_url'] })
-    rep.save
+  # Find an existing representative by ocdid (or build a new one), then populate
+  # it from the Geocodio payload. Re-searching a district updates the existing
+  # record instead of creating a duplicate.
+  def self.find_rep(official)
+    find_or_initialize_by(ocdid: official['govtrack_id']).update_from_geocodio(official)
+  end
+
+  def self.photo_url_for(bioguide_id)
+    return nil if bioguide_id.blank?
+
+    format(PHOTO_URL_TEMPLATE, bioguide_id[0], bioguide_id)
+  end
+
+  def self.geocodio_attributes(official)
+    bio = official['bio'] || {}
+    contact = official['contact'] || {}
+    social = official['social'] || {}
+    refs = official['references'] || {}
+
+    {
+      name: "#{bio['first_name']} #{bio['last_name']}".strip,
+      title: official['type'],
+      ocdid: official['govtrack_id'],
+      party: bio['party'],
+      bioguide_id: refs['bioguide_id'],
+      photo_url: photo_url_for(refs['bioguide_id']),
+      office_address: contact['address'],
+      phone: contact['phone'],
+      website: contact['url'],
+      contact_form: contact['contact_form'],
+      twitter: social['twitter'],
+      facebook: social['facebook'],
+      youtube: social['youtube']
+    }
   end
 
   def update_from_geocodio(official)
-    self.title = official['type']
-    self.ocdid = official['govtrack_id']
-    self.party = official['party']
-    self.photo_url = official['photo_url']
-    # TODO: store the address, phone and website
-    save!
+    update!(self.class.geocodio_attributes(official))
     self
   end
 end
