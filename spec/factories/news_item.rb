c26e96662c957843cba0162ee2b6f2bc1f3abec8
Victoria Moore <viamoore@berkeley.edu>
2026-08-14 07:04:07 +0000

    [test] fixed small test bug

--- spec/factories/news_item.rb
+++ spec/factories/news_item.rb
@@ -1,12 +1,24 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: news_items
+#
+#  id                :integer          not null, primary key
+#  description       :text
+#  link              :string           not null
+#  title             :string           not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  representative_id :integer          not null
+
 FactoryBot.define do
   factory :news_item do
     id { 10 }
     link { 'news.com/this-is-a-test' }
     title { 'Test News Story' }
     representative_id { 987_654 }
-    user
+    user_id { '0' }
   end
 
   factory :news_item_with_rep, class: 'NewsItem' do
@@ -14,5 +26,6 @@ FactoryBot.define do
     link { 'news.com/this-is-a-test' }
     title { 'Test News Story' }
     representative
+    user
   end
 end
